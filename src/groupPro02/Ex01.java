81fd35a00a3e15bc165d96f4881dc40b7cd77c2f
scity9799 <scity9799@gmail.com>
2026-01-14 09:27:16 +0900

    260114 09:27 최최종수정

--- src/groupPro02/Ex01.java
+++ src/groupPro02/Ex01.java
@@ -19,9 +19,7 @@ public class Ex01 {
 //			사용자 나이(age) 아동, 청소년, 성인
 //		상수선언 : 교통수단요금 / 교통수단별할인율
 //		사용자 나이 입력
-		
-		
-		
+				
 		Scanner sc= new Scanner (System.in);
 //		상수 선언 
 		final int BUS_PRICE = 1400; 	//버스요금 
@@ -35,9 +33,7 @@ public class Ex01 {
 		int age = 0; //나이 
 		String sAge = ""; //아동, 청소년, 성인 저장할 변수
 		int mileageIndex = 0; //마일리지의 탑승 순서를 알려주는 변수 
-		double mileageTotal = 0.0; //최종 마일리지 금액 
-		
-		
+		double mileageTotal = 0.0; //최종 마일리지 금액 		
 		int total = 0; //총 요금 
 		double avg = 0; //평균 요금
 		
@@ -73,7 +69,7 @@ public class Ex01 {
 //		서서울---------------------------------------
 //		반복문 (while문 사용)
 //
-//		사용한 교통수단 출력 (버스 또는 지하철)
+//		사용한 교통수단 출력 (버스 또는 지하철 또는 정보창)
 //
 //		교통수단 별 이용횟수(switch)
 //		- 버스일때()
@@ -111,13 +107,12 @@ public class Ex01 {
 //		김태우-------------------------------------
 //
 //		- 지하철일때()
-//switch(transpotation)<<<<<<머였던 거지? 차피 안쓰는 코드라 주석처리로 받긴함 무시하겠음
-// 입력방식을 바꿔볼 수 있을까? 난 삼항연산자 쓰겠음 if문 switch문 다 위에서 썻으니...
+
 			case 2 :
 				SUBWAY[s_count] =
-				age >= 0 && age < 12 ? 0 ://12살이상일때가 거짓문밑에있음
-					age >= 12 && age < 19 ? (int)(SUBWAY_PRICE * (1 - DISCOUNT_RATE)) ://19살이상일때 거짓문밑에
-						age >= 19 ? SUBWAY_PRICE :SUBWAY_PRICE ;//참도 거짓도 결과는 정해져있음.ㅋ
+				age < 12 ? 0 ://12살이상일때가 거짓문 밑에있음
+				age >= 12 && age < 19 ? (int)(SUBWAY_PRICE * (1 - DISCOUNT_RATE)) ://19살이상일때 거짓문밑에
+				age >= 19 ? SUBWAY_PRICE : SUBWAY_PRICE ;
 				
 				mileageCount[mileageIndex] = SUBWAY[s_count]; // 탄 순서대로 지하철 요금 저장 
 				mileageIndex++; 
@@ -138,17 +133,11 @@ public class Ex01 {
 					choice = sc.nextInt();
 					switch(choice){
 					case 1: System.out.println("현재까지 버스는 "+b_count+"회 이용하셨으며 지하철은 "+s_count+"회 이용하셨습니다\n총 교통수단 이용 횟수는: "+count+"회 입니다");break;
-					case 2:	if(sAge.equals("아동")) {System.out.println("현재 사용자는"+sAge+"이므로 교통비 무료 대상자입니다");}
+					case 2:	if(sAge.equals("아동")) {System.out.println("현재 사용자는 "+sAge+"이므로 교통비 무료 대상자입니다");}
 							else if (sAge.equals("청소년")){System.out.println("현재 사용자는 "+sAge+"이므로 교통비"+(DISCOUNT_RATE*100)+"% 할인 대상자 입니다.");}
 							else if(sAge.equals("성인")){System.out.println("현재 사용자는"+sAge+"이므로 나이 할인 대상자가 아닙니다.");}
 							break;
 					case 3: 
-						//김태우------------------------------------
-						//정원이가 작성한 코드
-						//System.out.println("현재 교통수단별 금액은\n버스: "+BUS_PRICE+"원\n지하철: "+SUBWAY_PRICE+"원 으로 설정되어 있습니다");break;
-						//이거 기반으로 해당 나이에 맞는 금액 알려주도록만 수정함
-						//나이대가 다르게 입력된채로 정보창에 들어왔는데 금액이 다 똑같이 알려주길래
-						//사용자의 정보에 맞는 금액 얼마인지 알수 있게끔만함
 						if(sAge.equals("아동")) {
 							System.out.println("현재 교통수단별 금액은\n버스: 0원\n지하철: 0원 으로 설정되어 있습니다");break;
 						}else if (sAge.equals("청소년")){
@@ -164,7 +153,6 @@ public class Ex01 {
 				}//switch문의 case3의 while문 끝
 				break;
 		//김태우----------------------------------------
-		//while문 탈출할거얌.
 			default : 
 				System.out.println("이용해주셔서 감사합니다");
 				cheack = false;
@@ -178,23 +166,17 @@ public class Ex01 {
 //		서정원--------------------
 		System.out.println("지하철 이용횟수: "+s_count);
 		System.out.println("버스 이용횟수: "+b_count);
-		System.out.println("총 교통수단 이용횟수: "+count);
-		
-		
-		
+		System.out.println("총 교통수단 이용횟수: "+count);		
 //		김진옥------------------------
 //		결제된 총 요금
 //
 //		평균요금
 //			
 		// 반복문을 사용해서 값을 꺼내고 요금 토탈값 구하기0.
-		  for (int i = 0 ; i <= s_count-1 ; i++) {
-		     
+		  for (int i = 0 ; i <= s_count-1 ; i++) {		     
 		         total += SUBWAY[i];
-		      }
-		  
-		  for (int i = 0 ; i <= b_count-1 ; i++) {
-		         
+		      }		  
+		  for (int i = 0 ; i <= b_count-1 ; i++) {		         
 		         total += BUS[i];
 		      }
 		  
@@ -205,9 +187,7 @@ public class Ex01 {
 	      // 2. 평균 요금 계산
 	      // 총 요금 / 총 이용 횟수
 	      //  이용 횟수가 0번일 때 나누면 에러가 나므로 조건문으로 확인해야 함
-	      
-	       
-
+	      	       
 	      if (count > 0) {
 	         // 정수 나누기 정수는 정수가 되므로, (double)로 형변환하여 소수점까지 계산
 	         avg = (double) total / count;
@@ -215,7 +195,7 @@ public class Ex01 {
 
 	      // 3. 평균 요금 출력
 	      // 실수로 출력 
-	 System.out.printf("평균 요금 : %.2f원\n", avg);
+	      System.out.printf("평균 요금 : %.2f원\n", avg);
 
 	      System.out.println("이용해 주셔서 감사합니다.");
 		
@@ -229,8 +209,7 @@ public class Ex01 {
 	      
 	      //구간 금액 누적합 
 	      for(int i =0; i <mileagenum2; i++) { // 마일리지 배열에 저장된 길이 
-	    	  mileageTotal +=mileageCount[i];
-	    	  
+	    	  mileageTotal +=mileageCount[i];	    	  
 	      }
 	      //10회 이상 사용시 5% 마일리지 적립 
 	      double mileage05 = mileageTotal * 0.05;
@@ -244,6 +223,6 @@ public class Ex01 {
 	      //총 마일리지
 	      System.out.printf("마일리지 금액 : %.0f\n", mileageTotal);
 	      sc.close();
-}
+	}
 }
 
