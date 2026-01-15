78256184939e475b421fd17bb2fa87263d115d5f
kjo000810boop <kjo000810@gmail.com>
2026-01-15 21:12:03 +0900

    260115 21:11 김진옥메소드 올림

--- src/groupPro02/Ex02.java
+++ src/groupPro02/Ex02.java
@@ -1,5 +1,41 @@
 package groupPro02;
+
+import java.util.Scanner;
+
 //메소드용 파일
+
 public class Ex02 {
+	// 김진옥 메소드 시작----------------------
+	String inputAge() {
+		Scanner sc = new Scanner(System.in);
+		int age = 0;
+		String sAge = "";
+
+		System.out.print("나이를 입력해주세요 : "); //정현주님 코드 복사
+		age = sc.nextInt();
+		sc.nextLine(); 
+
+		if (age >= 0) {
+			if (age >= 0 && age < 12) {
+				sAge = "아동";
+			} else if (age >= 12 && age < 19) {
+				sAge = "청소년";
+			} else {
+				sAge = "성인";
+			}
+		}
+		System.out.println(sAge + "입니다.");
+		
+		return sAge; // 결과값("아동", "청소년", "성인")을 main으로 던져줘서 출력 가능하게 함!
+	}
+//-----------김진옥 끝---------------------------------------------------------------------
+	public static void main(String[] args) {
+		
+				Ex02 ex = new Ex02(); 
+				//  메소드 실행 및 결과 받기 (sAge 변수에 결과가 들어감)
+				String sAge = ex.inputAge(); 
+				
+		
+	}
 
 }
